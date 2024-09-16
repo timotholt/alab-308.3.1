27b2102926159b37f11645f709d54f35f9bb9c7c
Timothy Otholt <timotholt@gmail.com>
2024-09-16 09:04:02 -0700

    Removed debugger statemetns as per questions asked in class, cleaned up code a little too

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ for (let n = 1; n <= 100; n++) {
     else if (divis5)
         console.log(`(n=${n}) Buzz`);
     else
-        console.log(`(n=${n}) No fizz or buzz`);
+        // console.log(`(n=${n}) No fizz or buzz`);
+    ;
 }
 
 //=========================================
@@ -36,7 +37,7 @@ for (let n = 1; n <= 100; n++) {
 console.log(`==================== Prime Number Check ====================`);
 
 // Fact: Next prime after 105 is 107
-n = 105;
+n = 727;
 
 // Start looking for prime numbers upwards from n
 let nextNumToCheckIfPrime = n + 1;
@@ -46,21 +47,18 @@ console.log(`Searching for a prime number higher than ${n}, starting with ${next
 /* keep looking until we find a prime number */
 primeNumSearch: while(true) {
 
-    // Assumption here, not fact
-    // let currentTestNumberIsPrime = true;
-
     // Start looking for a divisor less than current number
     let testDivisor = nextNumToCheckIfPrime - 1;
 
     // output debug message
-    console.log(`Testing divisor ${testDivisor} to see if ${nextNumToCheckIfPrime} is prime`);
+    // console.log(`Testing divisor ${testDivisor} to see if ${nextNumToCheckIfPrime} is prime`);
     
     // Only need to test divisors 2 and higher
     while ((testDivisor > 1) && (nextNumToCheckIfPrime % testDivisor !== 0)) {
         testDivisor--;
 
         // output debug message
-        console.log(`Testing divisor ${testDivisor} to see if ${nextNumToCheckIfPrime} is prime`);
+        // console.log(`Testing divisor ${testDivisor} to see if ${nextNumToCheckIfPrime} is prime`);
     }
 
     // If we found a prime number
@@ -68,7 +66,7 @@ primeNumSearch: while(true) {
         console.log(`Next prime number after ${n} is ${nextNumToCheckIfPrime}`);
         break primeNumSearch;
     } else {
-        console.log (`${nextNumToCheckIfPrime} is not prime.  Checking ${nextNumToCheckIfPrime+1}`);
+        // console.log (`${nextNumToCheckIfPrime} is not prime.  Checking ${nextNumToCheckIfPrime+1}`);
         
         // Keep looking upwards
         nextNumToCheckIfPrime++;
@@ -80,14 +78,13 @@ primeNumSearch: while(true) {
 //=========================================
 
 console.log(`==================== Feeling Loopy Part 1 ====================`);
-debugger;
+// debugger;
 
 const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-let cellArray = [``,``,``,``,``,``,``,``];
+let cellArray = [``,``,``,``];
 let cellNum = 0;
 let lastLineOutput = false;
-// let lastChar;
 
 /* Loop through every character */
 for (const c of csvData) {
@@ -96,17 +93,16 @@ for (const c of csvData) {
     if (c === `\n`) {
         console.log(cellArray[0], cellArray[1], cellArray[2], cellArray[3]);
         
-        for (i = 0; i < 8; i++)
+        for (i = 0; i < cellArray.length; i++)
             cellArray[i] = ``;
 
         cellNum = 0;
         lastLineOutput = true;
         continue;
-        }
+    }
 
     /* Look for comma, if so advance to next cell */
     if (c === `,`) {
-        lastChar = c;
         cellNum++;
         continue;
     }
@@ -120,11 +116,11 @@ if (!lastLineOutput)
     console.log(cellArray[0], cellArray[1], cellArray[2], cellArray[3]);
 
 console.log(`==================== Feeling Loopy Part 2 ====================`);
-debugger;
+// debugger;
 
 const csvData2 = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`;
 
-let cellArray2 = [``,``,``,``,``,``,``,``];
+let cellArray2 = [``,``,``,``];
 let cellNum2 = 0;
 let lastLineOutput2 = false;
 
@@ -135,7 +131,7 @@ for (const c of csvData2) {
     if (c === `\n`) {
         console.log(cellArray2[0], cellArray2[1], cellArray2[2], cellArray2[3]);
         
-        for (let i = 0; i < 8; i++)
+        for (let i = 0; i < cellArray2.length; i++)
             cellArray2[i] = ``;
 
         cellNum2 = 0;
